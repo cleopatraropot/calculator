04049bcdfe9eacf0445aba721fbcbb0982bd407f
cleopatraropot <cleopatraropot@gmail.com>
2024-06-11 14:55:46 +0300

    Make the calculator work (not done yet)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
-import { CalculatorButton } from './components';
+import {CalculatorButton, CalculatorDisplay} from './components';
+import { evaluate } from 'mathjs';
 
 function App() {
+  const [displayValue, setDisplayValue] = useState('');
+  const [result, setResult] = useState('');
+
+
+  const handleButtonClick = (text: string) => {
+    if (text === '=') {
+      try {
+        const evaluationResult = evaluate(displayValue);
+        setDisplayValue(evaluationResult.toString());
+        setResult(evaluationResult.toString());
+      } catch (error) {
+        setDisplayValue('Error');
+        setResult('');
+      }
+    } else if (text === 'C') {
+      setDisplayValue('');
+      setResult('');
+    } else if (text === 'DEL') {
+      setDisplayValue((prevValue) => prevValue.slice(0, -1));
+    } else {
+      setDisplayValue((prevValue) => prevValue + text);
+    }
+  };
+
   return (
       <div>
         <div className="container">
@@ -10,30 +35,27 @@ function App() {
           <h2>Use this :)</h2>
           <div className="iphone-frame">
             <div className="calculator">
-              <div className="right-content-number">
-                <p className="calculate">42*12</p>
-                <p className="result">504</p>
-              </div>
+              <CalculatorDisplay value={displayValue} result={result}/>
               <div className="el-group">
-                <CalculatorButton text="0" color="#3E4047" shape="oval"/>
-                <CalculatorButton text="." color="#3E4047" shape="round"/>
-                <CalculatorButton text="=" color="#EA9B3E" shape="round"/>
-                <CalculatorButton text="1" color="#3E4047" shape="round"/>
-                <CalculatorButton text="2" color="#3E4047" shape="round"/>
-                <CalculatorButton text="3" color="#3E4047" shape="round"/>
-                <CalculatorButton text="+" color="#EA9B3E" shape="round"/>
-                <CalculatorButton text="4" color="#3E4047" shape="round"/>
-                <CalculatorButton text="5" color="#3E4047" shape="round"/>
-                <CalculatorButton text="6" color="#3E4047" shape="round"/>
-                <CalculatorButton text="-" color="#EA9B3E" shape="round"/>
-                <CalculatorButton text="7" color="#3E4047" shape="round"/>
-                <CalculatorButton text="8" color="#3E4047" shape="round"/>
-                <CalculatorButton text="9" color="#3E4047" shape="round"/>
-                <CalculatorButton text="x" color="#EA9B3E" shape="round"/>
-                <CalculatorButton text="C" color="#C1C1C1" shape="round"/>
-                <CalculatorButton text="DEL" color="#C1C1C1" shape="round"/>
-                <CalculatorButton text="%" color="#C1C1C1" shape="round"/>
-                <CalculatorButton text="/" color="#EA9B3E" shape="round"/>
+                <CalculatorButton text="0" color="#3E4047" shape="oval" onClick={handleButtonClick}/>
+                <CalculatorButton text="." color="#3E4047" shape="round" onClick={handleButtonClick}/>
+                <CalculatorButton text="=" color="#EA9B3E" shape="round" onClick={handleButtonClick}/>
+                <CalculatorButton text="1" color="#3E4047" shape="round" onClick={handleButtonClick}/>
+                <CalculatorButton text="2" color="#3E4047" shape="round" onClick={handleButtonClick}/>
+                <CalculatorButton text="3" color="#3E4047" shape="round" onClick={handleButtonClick}/>
+                <CalculatorButton text="+" color="#EA9B3E" shape="round" onClick={handleButtonClick}/>
+                <CalculatorButton text="4" color="#3E4047" shape="round" onClick={handleButtonClick}/>
+                <CalculatorButton text="5" color="#3E4047" shape="round" onClick={handleButtonClick}/>
+                <CalculatorButton text="6" color="#3E4047" shape="round" onClick={handleButtonClick}/>
+                <CalculatorButton text="-" color="#EA9B3E" shape="round" onClick={handleButtonClick}/>
+                <CalculatorButton text="7" color="#3E4047" shape="round" onClick={handleButtonClick}/>
+                <CalculatorButton text="8" color="#3E4047" shape="round" onClick={handleButtonClick}/>
+                <CalculatorButton text="9" color="#3E4047" shape="round" onClick={handleButtonClick}/>
+                <CalculatorButton text="x" color="#EA9B3E" shape="round" onClick={handleButtonClick}/>
+                <CalculatorButton text="C" color="#C1C1C1" shape="round" onClick={handleButtonClick}/>
+                <CalculatorButton text="DEL" color="#C1C1C1" shape="round" onClick={handleButtonClick}/>
+                <CalculatorButton text="%" color="#C1C1C1" shape="round" onClick={handleButtonClick}/>
+                <CalculatorButton text="/" color="#EA9B3E" shape="round" onClick={handleButtonClick}/>
               </div>
             </div>
           </div>
